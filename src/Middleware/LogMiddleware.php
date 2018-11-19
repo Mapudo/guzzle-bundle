e07f10178cc1a34150cfbe3eec68fd5f40c3692b
Christian Jost <christian.jost@mapudo.com>
2018-11-19 14:51:53 +0100

    MAP-2890 As a Developer, I have detailed information about running Akeneo imports, so that I can more precisely identify any problems
    * Modify `changelog` regarding the `duration`

--- src/Middleware/LogMiddleware.php
+++ src/Middleware/LogMiddleware.php
@@ -93,7 +93,7 @@ class LogMiddleware
         return $this;
     }
 
-    private function buildContext(RequestInterface $request, ResponseInterface $response, float $duration): array
+    private function buildContext(RequestInterface $request, ResponseInterface $response, float $duration = null): array
     {
         $request->getBody()->rewind();
         $context = [
